a27f9e914ed64a63f61069bc18096c5c8f75045d
Anggia Intan <intananggia23@gmail.com>
2024-10-13 00:05:24 +0700

    membuat set read-only

--- app/src/main/java/com/example/questbasickotlin_0203/collections.kt
+++ app/src/main/java/com/example/questbasickotlin_0203/collections.kt
@@ -1,12 +1,18 @@
 package com.example.questbasickotlin_0203
 
+// List adalah kumpulan data yang disusun secaramberurutan dari
+// List bersifat ordered, artinya data yang dimasukkan akan dimiliki posisi sesuai aturan
+// List bersifat mutable dan read only, artinya data yang dimasukkan dapat diubah dan tidak dapat diubah
+// List Read-Only menggunakan listOf
+// List Mutable menggunakan mutableListOf
+
 fun ContohList() {
     println("=== List ===")
     // List Read-Only
     val readOnlyAbjad = listOf("A", "B", "C")
     println(readOnlyAbjad)
 
-    // Map mutable
+    // List mutable
     val shape: MutableList<String> = mutableListOf("Circle", "Square", "Triangle")
     println(shape)
 
@@ -18,7 +24,7 @@ fun ContohList() {
     shape.remove("Triangle")
     println(shape)
 
-    // Mengubah data di dalam List mutable
+    // Mengubah data di dalam List Mutable
     shape[0] = "Oval"
     println(shape)
 
@@ -26,6 +32,22 @@ fun ContohList() {
     val shapesLocked: List<String> = shape
     println(shapesLocked)
 }
+
+// Set adalah kumpulan data yang tidak memiliki posisi atau urutan tertentu dan hanya menyimpan data unik (tidak ada duplikasi)
+// Set bersifat unordered, artinya data yang dimasukkan tidak memiliki posisi tertentu
+// Set bersifat mutable dan read only, artinya data yang dimasukkan dapat diubah dan tidak dapat diubah
+// Set Read-Only menggunakan setOf
+// Set Mutable menggunakan mutableSetOf
+
+fun ContohSet() {
+    println()
+    println("=== Set ===")
+}
+
+// Set Read-Only
+val readOnlyAbjad = setOf("A", "B", "C")
+println(readOnlyAbjad)
+
 
 fun main() {
     ContohList()
